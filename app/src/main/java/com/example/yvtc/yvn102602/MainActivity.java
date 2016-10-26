854a5f25e29cb6d8a944b4473e783b89cd436c32
shaojung <shaojung@gmail.com>
2016-10-26 20:50:27 +0800

    add and delete

--- app/src/main/java/com/example/yvtc/yvn102602/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn102602/MainActivity.java
@@ -2,23 +2,42 @@ package com.example.yvtc.yvn102602;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    String fruits[] = {"芭樂", "芒果", "荔枝", "甘蔗", "鳳梨"};
+    ArrayList<String> mylist;
     Spinner spinner;
     ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mylist = new ArrayList<>();
+        mylist.add("芭樂");
+        mylist.add("芒果");
+        mylist.add("荔枝");
         setContentView(R.layout.activity_main);
         spinner = (Spinner) findViewById(R.id.spinner);
 
         adapter = new ArrayAdapter<String>(MainActivity.this,
                             android.R.layout.simple_spinner_item,
-                            fruits);
+                            mylist);
         spinner.setAdapter(adapter);
 
+    }
+    public void clickAdd(View v)
+    {
+        EditText ed = (EditText) findViewById(R.id.editText);
+        String str = ed.getText().toString();
+        mylist.add(str);
+    }
+    public void clickDelete(View v)
+    {
+        mylist.remove(spinner.getSelectedItemPosition());
+        adapter.notifyDataSetChanged();
     }
 }
